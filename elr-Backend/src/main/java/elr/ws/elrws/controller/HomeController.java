d343c2a27e7ccb9c6f499150246d9f270b201e59
Brain Stone <16119519+Stonerville@users.noreply.github.com>
2018-06-23 14:45:28 -0400

    Updated Backend: docker images gs-spring-boot:latestDockerfile & pom

---
+++ elr-Backend/src/main/java/elr/ws/elrws/controller/HomeController.java
@@ -0,0 +1,17 @@
+package elr.ws.elrws.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+}
+
